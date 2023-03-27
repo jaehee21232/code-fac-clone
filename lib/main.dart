617d46691d59becc5ee83cc2556da2b697cb236e
jaehee21232 <a62533196@gmail.com>
2023-03-27 14:25:51 +0900

    feat: TextFormField Test

---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:code_fac/common/const/component/custom_text_form_field.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    _App(),
+  );
+}
+
+class _App extends StatelessWidget {
+  const _App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+          backgroundColor: Colors.white,
+          body: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CustomTextFormField(
+                hintText: "이메일을 입력해주세요",
+                onChanged: (value) {},
+              ),
+              CustomTextFormField(
+                hintText: "비밀번호를 입력해주세요",
+                onChanged: (value) {},
+                obscureText: true,
+              ),
+            ],
+          )),
+    );
+  }
+}
