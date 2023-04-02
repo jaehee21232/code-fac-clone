a9e92dac7240523b8c241f51ac2e50a0c6fcad6c
jaehee21232 <a62533196@gmail.com>
2023-04-02 19:09:46 +0900

    feat: bottom appbar

--- lib/common/layout/deafult_layout.dart
+++ lib/common/layout/deafult_layout.dart
@@ -2,13 +2,43 @@ import 'package:flutter/material.dart';
 
 class DefaultLayout extends StatelessWidget {
   final Widget child;
-  const DefaultLayout({super.key, required this.child});
+  final Color? backgroundColor;
+  final String? title;
+  final Widget? bottomNavigationBar;
+  const DefaultLayout(
+      {super.key,
+      required this.child,
+      this.backgroundColor,
+      this.title,
+      this.bottomNavigationBar});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: renderAppBar(),
       body: child,
-      backgroundColor: Colors.white,
+      backgroundColor: backgroundColor ?? Colors.white,
+      bottomNavigationBar: bottomNavigationBar,
     );
+  }
+
+  AppBar? renderAppBar() {
+    if (title == null) {
+      return null;
+    } else {
+      return AppBar(
+        title: Text(
+          title!,
+          style: TextStyle(
+            fontSize: 16,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+        elevation: 0,
+        backgroundColor: Colors.white,
+        foregroundColor: Colors.black,
+        centerTitle: true,
+      );
+    }
   }
 }
--- lib/common/view/root_tab.dart
+++ lib/common/view/root_tab.dart
@@ -1,13 +1,59 @@
+import 'package:code_fac/common/const/colors.dart';
+import 'package:code_fac/common/layout/deafult_layout.dart';
 import 'package:flutter/material.dart';
 
-class RootTab extends StatelessWidget {
+class RootTab extends StatefulWidget {
   const RootTab({super.key});
 
   @override
+  State<RootTab> createState() => _RootTabState();
+}
+
+class _RootTabState extends State<RootTab> {
+  int index = 0;
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text("Root 탭"),
+    return DefaultLayout(
+      title: "코펙 딜리버리",
+      child: Scaffold(
+        body: Center(
+          child: Text("Root 탭"),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          selectedItemColor: PRIMARY_COLOR,
+          unselectedItemColor: BODY_TEXT_COLOR,
+          selectedFontSize: 10.0,
+          unselectedFontSize: 10.0,
+          type: BottomNavigationBarType.fixed,
+          onTap: (int index) {
+            setState(() {
+              this.index = index;
+            });
+          },
+          currentIndex: index,
+          items: [
+            BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.home_outlined,
+                ),
+                label: "홈"),
+            BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.fastfood_outlined,
+                ),
+                label: "음식"),
+            BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.receipt_long_outlined,
+                ),
+                label: "주문"),
+            BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.person_outline,
+                ),
+                label: "프로필"),
+          ],
+        ),
       ),
     );
   }
