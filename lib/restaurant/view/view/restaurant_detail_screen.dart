a1cbff82eb99e066113fb7997aa4d77e5ae6e8e9
jaehee21232 <a62533196@gmail.com>
2023-04-05 15:11:54 +0900

    feat: 레스토랑디테일스크린 레이아웃

---
+++ lib/restaurant/view/view/restaurant_detail_screen.dart
@@ -0,0 +1,11 @@
+import 'package:code_fac/common/layout/deafult_layout.dart';
+import 'package:flutter/material.dart';
+
+class RestaurantDetailScreen extends StatelessWidget {
+  const RestaurantDetailScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultLayout(child: child);
+  }
+}
